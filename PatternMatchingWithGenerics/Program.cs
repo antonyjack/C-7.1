5433a13e01c4255439f3a39c6600ebd8054c53c4
Antonysamy <antonysamy931@outlook.com>
2019-10-14 19:47:34 +0530

    Pattern matching on generic type parameters

---
+++ PatternMatchingWithGenerics/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace PatternMatchingWithGenerics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintDetails<Student>(new Student()
+            {
+                Name = "Antony"
+            });
+
+            PrintDetails<Employee>(new Employee()
+            {
+                Name = "Antony"
+            });
+            
+            void PrintDetails<T>(T detail) where T : class
+            {
+                switch(detail)
+                {
+                    case Student student:
+                        Console.WriteLine($"Student name : {student.Name}.");
+                        break;
+                    case Employee employee:
+                        Console.WriteLine($"Employee name : {employee.Name}.");
+                        break;
+                }
+            }
+        }
+
+        class Common
+        {
+            public string Name { get; set; }
+            public int Age { get; set; }
+        }
+
+        class Student : Common { }
+
+        class Employee : Common { }
+    }
+    
+}
--- README.md
+++ README.md
@@ -8,3 +8,6 @@
 
 ## Inferred Tuple element names
 > The names of tuple elements can be inferred from the variables used to initialize the tuple. [Reference](https://github.com/antonyjack/C-7.1/blob/master/InferredTuple/README.md)
+
+## Pattern matching on generic type parameters
+> Pattern matching feature already exists in C# 7.0. In C# 7.1 the pattern expression for `is` and `switch` type pattern may have the type of a generic type parameter. [Reference](https://github.com/antonyjack/C-7.1/blob/master/PatternMatchingWithGenerics)
